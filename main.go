4261623e2a7672c38e7140109addcfc99fc1274c
Jefferson Campos <jefferson@jeffersoncampos.eti.br>
2026-01-02 03:50:13 -0300

    feat: Basic Program.
    Hello world

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var pizzashop_name string = "Go Pizza Shop"
+	instagram, whatsapp := "@go_pizzashop", 15994785333
+	fmt.Println(pizzashop_name, instagram, whatsapp)
+}
